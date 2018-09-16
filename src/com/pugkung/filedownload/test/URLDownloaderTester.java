d26fe6a4ebe4b955549f1853e0f32c8dd7e6cc35
Tanapol Rattanapichetkul <tanapol.raa@gmail.com>
2018-09-16 20:20:28 +0700

    isolate large file test case as special test

--- src/com/pugkung/filedownload/test/URLDownloaderTester.java
+++ src/com/pugkung/filedownload/test/URLDownloaderTester.java
@@ -13,7 +13,6 @@ import static org.mockito.Mockito.verify;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.net.HttpURLConnection;
 import java.net.SocketTimeoutException;
 import java.net.URL;
@@ -121,49 +120,5 @@ public class URLDownloaderTester {
 		verify(fd, times(1)).downloadFromURL(any(URL.class), eq(expectedFile));
 		assertEquals(status,DownloaderStatus.IO_ERROR);
 		assertFalse(expectedFile.exists());
-	}
-	
-	@Test
-	public void TestURLDownloader_testLargeFile() throws Exception {
-		String testURL = "http://localhost/testfile.out";
-		String outputPath = "";
-		
-		fd = spy(new URLDownloader(testURL, outputPath));
-		
-		String outputFile = outputPath + fd.generateOutputFileName(testURL);
-		File expectedFile = new File(outputFile);
-		expectedFile.deleteOnExit();
-		
-		URL mockURL = PowerMockito.mock(URL.class);
-		
-		// Generate very large file as data stream
-		String mockFileName = "mockFile.in";
-		RandomAccessFile f = new RandomAccessFile(mockFileName, "rw");
-        f.setLength(1024 * 1024 * 1024);
-        
-        doAnswer(new Answer<RandomAccessFile>() {
-			public RandomAccessFile answer(InvocationOnMock invocation) throws Throwable {
-				RandomAccessFile mockFile = new RandomAccessFile(expectedFile,"rw");
-				try {
-					mockFile.seek(0);
-			        for(int i = 0; i < f.length(); i++) {
-			        	mockFile.write(f.read());
-			        }
-			        } catch(IOException ex) {
-			        	//it's EOF
-			        }
-				return mockFile;
-			}}).when(fd).downloadFromURL(any(URL.class), eq(expectedFile));
-		
-		fd.downloadFromURL(mockURL, expectedFile);
-		
-		f.close();
-		assertTrue(expectedFile.exists());
-		
-		// cleanup test data
-		File file1 = new File(mockFileName);
-		File file2 = new File(outputFile);
-		file1.deleteOnExit();
-		file2.deleteOnExit();
 	}
 }
